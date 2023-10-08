a750995a6be3bbc9734c5273726da545d20f9975
Roman Kopaliani <roman.kopaliani@gmail.com>
2023-10-08 18:57:42 +0300

    Scaffolding
    - Rename targets.
    - Integrate SwiftLint.
    - Integrate Swinject, CombineExt.
    - Update SwiftLint to handle headers.

--- Frontend/FrontendApp.swift
+++ Frontend/FrontendApp.swift
@@ -1,14 +1,15 @@
 //
-//  OrionApp.swift
-//  orion-assignment
+//  FrontendApp.swift
+//  Frontend
 //
 //  Created by Roman Kopaliani on 08.10.2023.
+//  Copyright © 2023 orion-assigment. All rights reserved.
 //
 
 import SwiftUI
 
 @main
-struct OrionApp: App {
+struct FrontendApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
---
+++ FrontendUITests/FrontendUITests.swift
@@ -0,0 +1,33 @@
+//
+//  FrontendUITests.swift
+//  FrontendUITests
+//
+//  Created by Roman Kopaliani on 08.10.2023.
+//  Copyright © 2023 orion-assigment. All rights reserved.
+//
+
+import XCTest
+
+final class FrontendUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+        let app = XCUIApplication()
+        app.launch()
+    }
+
+    func testLaunchPerformance() throws {
+        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
+            // This measures how long it takes to launch your application.
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
+                XCUIApplication().launch()
+            }
+        }
+    }
+}
