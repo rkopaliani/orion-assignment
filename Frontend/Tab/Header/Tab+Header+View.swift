47219271ba4bd1d648209ead0f9117e27ee20161
Roman Kopaliani <roman.kopaliani@gmail.com>
2023-10-24 10:02:25 +0300

    Disable redundant animations

--- Frontend/Tab/Header/Tab+Header+View.swift
+++ Frontend/Tab/Header/Tab+Header+View.swift
@@ -22,7 +22,7 @@ extension Tab.Header {
 
                     Tab.Header.Item.View(viewModel: $0)
                 }
-                .animation(.easeIn(duration: 0.4), value: viewModel.tabVMs)
+//                .animation(.easeIn(duration: 0.4), value: viewModel.tabVMs)
             }
 
         } // body
